9ff66f2c475f410885287265287a30a6689a2a3a
Matheus2910 <110101691+Matheus2910@users.noreply.github.com>
2024-05-13 09:11:08 -0300

    Projeto Dia 13/05/24

---
+++ Projeto1_MatheusAquino/script.js
@@ -0,0 +1,5 @@
+document.getElementById('peru').addEventListener('click',function() {
+    const mensagem = document.getElementById 
+    ('mensagem'); mensagem.style.color = mensagem.style.color ===
+    'blue' ? 'red' : 'blue';
+});
